6f9a88bfdbd493d49c8195b2919b131d89ae0c2a
Nicolas Bonfante <nicolas.bonfante@movesol.com>
2022-02-06 19:07:45 +0100

    Download the grid of today

--- src/mot_du_jour.rs
+++ src/mot_du_jour.rs
@@ -1,7 +1,12 @@
 use reqwest;
+use chrono::{DateTime, Utc};
 
 pub fn mot_du_jour() -> String {
-    let response = reqwest::blocking::get("https://sutom.nocle.fr/mots/30.txt")
+
+    let aujourdhui: DateTime<Utc> = Utc::now();
+    let origine = DateTime::parse_from_rfc3339("2022-01-08T00:00:00-08:00").unwrap().with_timezone(&Utc);
+    let numero_grille = (aujourdhui - origine).num_days() + 1;
+    let response = reqwest::blocking::get(format!("https://sutom.nocle.fr/mots/{}.txt", numero_grille))
         .unwrap()
         .text();
     return response.unwrap();
