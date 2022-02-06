ef218153b1ebb079fa860987dc84ccac8c15d060
Bonfante Nicolas <nicolas.bonfante@movesol.com>
2022-02-06 21:44:59 +0100

    Ajout de la couleur pour tous et ajout des lettres mal placees.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod mot_du_jour;
 use std::io;
 use std::io::Write;
 use std::{thread, time};
+use std::collections::HashMap;
+use termion::{color, style};
 
 fn uppercase(name: &mut String) {
     let length = name.len();
@@ -14,7 +16,21 @@ fn uppercase(name: &mut String) {
     }
 }
 
-fn verifie_mot(mot_joueur: &String, mot_du_jour: &String) -> Result<bool, &'static str> {
+fn frequences_lettre(mot_joueur: &String) -> HashMap<char, i64> {
+    let mut frequences = HashMap::new();
+    for idx in 0x41..0x5B {
+        frequences.insert(idx as u8 as char, 0);
+    }
+
+    for c in mot_joueur.chars() {
+        let count = frequences.entry(c).or_insert(0);
+        *count += 1;
+    }
+
+    return frequences;
+}
+
+fn verifie_mot(mot_joueur: &String, mot_du_jour: &String, frequences: &HashMap<char, i64>) -> Result<bool, &'static str> {
     let long_joueur = mot_joueur.chars().count();
     let long_jour = mot_du_jour.chars().count();
 
@@ -27,15 +43,24 @@ fn verifie_mot(mot_joueur: &String, mot_du_jour: &String) -> Result<bool, &'stat
     }
 
     let mut error = 0;
+    let mut local_frequences = frequences.clone();
     for idx in 0..long_jour {
-        if mot_du_jour.chars().nth(idx) == mot_joueur.chars().nth(idx) {
-            print!("{}", "🟥");
-            // TODO: 🟡
-            // Implémenter les lettres mal placées.
+        let c = mot_joueur.chars().nth(idx).unwrap();
+        let nb_occurrence = local_frequences.get(&c).unwrap();
+        if mot_du_jour.chars().nth(idx).unwrap() == c {
+            print!("{}{}{}", color::Bg(color::Red), c, color::Bg(color::Reset));
+            let count = local_frequences.entry(c).or_insert(0);
+            *count -= 1;
+        } else if nb_occurrence > &0 {
+            print!("{}{}{}{}{}", color::Fg(color::Black), color::Bg(color::LightYellow), c, color::Bg(color::Reset), color::Fg(color::Reset));
+            let count = local_frequences.entry(c).or_insert(0);
+            *count -= 1;
+            error += 1
         } else {
             error += 1;
-            print!("{}", '🟦');
+            print!("{}{}{}", color::Bg(color::Blue), c, color::Bg(color::Reset));
         }
+        print!(" ");
         let ten_millis = time::Duration::from_millis(100);
         thread::sleep(ten_millis);
         std::io::stdout().flush().unwrap();
@@ -46,7 +71,9 @@ fn verifie_mot(mot_joueur: &String, mot_du_jour: &String) -> Result<bool, &'stat
 }
 
 fn main() {
+    print!("{}", style::Bold);
     let mot_du_jour = mot_du_jour::mot_du_jour();
+    let frequences = frequences_lettre(&mot_du_jour);
 
     for idx in 0..mot_du_jour.chars().count() {
         if idx == 0 {
@@ -64,7 +91,7 @@ fn main() {
         uppercase(& mut buffer);
         buffer = buffer[..buffer.chars().count() - 1].to_owned();
         
-        let res = verifie_mot(&buffer, &mot_du_jour);
+        let res = verifie_mot(&buffer, &mot_du_jour, &frequences);
         match res {
             Ok(success) => {
                 if success {
@@ -87,4 +114,5 @@ fn main() {
             break;
         }
     }
+    print!("{}", style::NoBold);
 }
